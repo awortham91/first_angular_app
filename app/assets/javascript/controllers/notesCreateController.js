74bdc9a0a6b42d31d94608fe492523d829dd86e6
Andrew Wortham <awortham@worthams.net>
2015-08-08 11:21:24 -0400

    notes are good, onto users

--- app/assets/javascript/controllers/notesCreateController.js
+++ app/assets/javascript/controllers/notesCreateController.js
@@ -1,3 +1,13 @@
-angular.module('Fun').controller('NotesCreateController', function(){
+angular.module('Fun').controller('NotesCreateController', function($scope, Note, $location){
+  $scope.note = new Note();
+  $scope.isSubmitting = false;
 
+  $scope.saveNote = function(note){
+    $scope.isSubmitting
+    note.$save().then(function(){
+      $location.path("/notes");
+    }).finally(function(){
+      $scope.isSubmitting = false;
+    });
+  }
 });
--- app/assets/javascript/controllers/notesShowController.js
+++ app/assets/javascript/controllers/notesShowController.js
@@ -1,3 +1,9 @@
-angular.module('Fun').controller('NotesShowController', function(Note, $scope, $routeParams){
+angular.module('Fun').controller('NotesShowController', function(Note, $scope, $routeParams, $location){
   $scope.note = Note.get({id: $routeParams.id});
+
+  $scope.deleteNote = function(note) {
+    note.$remove().then(function(){
+      $location.path('/notes')
+    });
+  }
 });
