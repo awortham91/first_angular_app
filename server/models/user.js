c5b1734586e51dd7c75f221aaebf42c266a05be9
Andrew Wortham <awortham@worthams.net>
2015-08-15 17:37:31 -0400

    styles and seeds

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
 var _ = require('lodash');
 
 var users = [
-  {"id": 1,  "email": "jeffrey@zeldman.com", "username": "zeldman", "name": "Prince Hamlet of Denmark", "bio": "Founder, Happy Cog studios. Author, Designing With Web Standards. Publisher, A List Apart, A Book Apart.", "twitter_handle": "@zeldman", "site": "zeldman.com"},
+  {"id": 1,  "email": "jeffrey@zeldman.com", "username": "zeldman", "name": "Hamlet", "bio": "Founder, Happy Cog studios. Author, Designing With Web Standards. Publisher, A List Apart, A Book Apart.", "twitter_handle": "@zeldman", "site": "zeldman.com"},
   {"id": 2,  "email": "brad@bradly.com", "username": "b_green", "name": "Iago", "bio": "I work at Google where I manage AngularJS and Google's internal sales productivity applications. I'm a dad.", "twitter_handle": "@bradlygreen", "site": "google.com/+BradGreen"},
-  {"id": 3,  "email": "", "username": "Meyer the Eric", "name": "Eric A. Meyer", "bio": "Web standards | (X)HTML | CSS | microformats | community | writing | speaking | signing man.", "twitter_handle": "@meyerweb", "site": "meyerweb.com"},
+  {"id": 3,  "email": "", "username": "Meyer the Eric", "name": "King Lear", "bio": "Web standards | (X)HTML | CSS | microformats | community | writing | speaking | signing man.", "twitter_handle": "@meyerweb", "site": "meyerweb.com"},
   {"id": 4,  "email": "gregg@codeschool.com", "username": "GP", "name": "Gregg Pollack", "bio": "Founder of Envy Labs, Code School, Orlando Ruby Users Group, BarCamp Orlando, and the Orlando Tech Events newsletter.", "twitter_handle": "@greggpollack", "site": "EnvyLabs.com"},
   {"id": 5,  "email": "me@rachelhigley.com", "username": "r_higley", "name": "Rachel Higley", "bio": "A web developer located in central florida", "twitter_handle": "@RachelHigley", "site": ""},
   {"id": 6,  "email": "mail@zdn.me", "username": "zach", "name": "Zachary Nicoll", "bio": "Bio sections always intimidate me. I can never think of anything to say that will achieve that awe inspiring effect I want it to have.", "twitter_handle": "@turtleguyy", "site": "zacharynicoll.com"},
