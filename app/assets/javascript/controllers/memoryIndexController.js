5cdb9c69df383c9a280d08790b3a8bae4b091c7b
Andrew Wortham <awortham@worthams.net>
2015-08-16 23:35:11 -0400

    WIP memory logic

--- app/assets/javascript/controllers/memoryIndexController.js
+++ app/assets/javascript/controllers/memoryIndexController.js
@@ -3,17 +3,40 @@ angular.module('Fun').controller('MemoryIndexController', function(User, Note, $
   $scope.notes = Note.query();
   $scope.toggled = false;
   $scope.ready = false;
+  var chosenCards = []
+  var matchedCards = []
+
+  function checkForCards(noteId){
+    for (var i = 0, len = chosenCards.length; i < len; i++) {
+      if (chosenCards[i] === noteId) {
+          return false;
+      };
+    };
+    for (var i = 0, len = chosenCards.length; i < len; i++) {
+      if (matchedCards[i] === noteId) {
+          return false;
+      };
+    };
+    return true
+  }
 
   $scope.toggleTime = function(cards) {
     if(!$scope.toggled){
       var card = Note.get({id: cards}, function() {
-        document.getElementById(cards).innerHTML = card["description"]
-        $scope.toggled = true;
+        if(checkForCards(card.id)){
+          document.getElementById(cards).innerHTML = card["description"]
+          $scope.toggled = true;
+          chosenCards.push(card.id)
+        };
       });
     } else if($scope.toggled && !$scope.ready) {
       var card = Note.get({id: cards}, function() {
-        document.getElementById(cards).innerHTML = card["description"]
-        $scope.ready = true;
+        if(checkForCards(card.id)){
+          document.getElementById(cards).innerHTML = card["description"]
+          $scope.ready = true;
+          var firstCard = Note.get({id: cards}
+          chosenCards = []
+        };
       });
     } else {
       x = document.getElementsByClassName("memory_index_card_quote")
